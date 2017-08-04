8466bb4f87390e90ab68410d1d6accfafedb4315
Ben Budinger <budingerbc@gmail.com>
2017-08-04 09:03:39 -0700

    MVC and Contact class structure setup.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Contacts.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Contacts.Contollers
+{
+  public class HomeController : Controller
+  {
+
+  }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace Contacts.Models
+{
+  public class Contact
+  {
+    private string _firstName;
+    private string _lastName;
+    private string _phoneNumber;
+    private string _address;
+
+    public Contact()
+    {
+      
+    }
+  }
+}
