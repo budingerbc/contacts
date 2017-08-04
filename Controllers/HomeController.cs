3eeff4ebd268f5574e6b286b27b54341d4febe2b
Ben Budinger <budingerbc@gmail.com>
2017-08-04 10:17:33 -0700

    Add getters and setters for Contact class, as well as initial routing in the Controller for the index, contact form, and new contact pages.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,6 +7,24 @@ namespace Contacts.Contollers
 {
   public class HomeController : Controller
   {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      List<Contact> contacts = Contact.GetAll();
+      return View(contacts);
+    }
 
+    [HttpGet("/contact/form")]
+    public ActionResult AddContact()
+    {
+      return View();
+    }
+
+    [HttpPost("/contact/new")]
+    public ActionResult NewContact()
+    {
+      Contact newContact = new Contact(Request.Form["first-name"], Request.Form["last-name"], Request.Form["phone-number"], Request.Form["address"]);
+      return View(newContact);
+    }
   }
 }
--- Models/Contact.cs
+++ Models/Contact.cs
@@ -8,10 +8,46 @@ namespace Contacts.Models
     private string _lastName;
     private string _phoneNumber;
     private string _address;
+    private int _id;
+    private static List<Contact> _contacts = new List<Contact> {};
 
-    public Contact()
+    public Contact(string first, string last, string phone, string address)
     {
-      
+      _firstName = first;
+      _lastName = last;
+      _phoneNumber = phone;
+      _address = address;
+      _contacts.Add(this);
+      _id = _contacts.Count;
+    }
+
+    public string GetFirstName()
+    {
+      return _firstName;
+    }
+    public string GetLastName()
+    {
+      return _lastName;
+    }
+    public string GetPhoneNumber()
+    {
+      return _phoneNumber;
+    }
+    public string GetAddress()
+    {
+      return _address;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public static List<Contact> GetAll()
+    {
+      return _contacts;
+    }
+    public void SetId(int id)
+    {
+      _id = id;
     }
   }
 }
